000f12eda2ccf4c4a45a59f1a0e8adc381e6984e
Mohamed Badawy <mohamed.badawy@5d-vr.com>
2025-04-17 19:16:22 +0200

    🔡

---
+++ Assets/Scripts/First_Interactable.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+
+public class First_Interactable : MonoBehaviour {
+
+    public static First_Interactable Instance;
+
+    public Material cube1;
+    public Material cube2;
+    [SerializeField] private GameObject above_Txt;
+    [SerializeField] private GameObject bottom_Txt;
+    [SerializeField] private GameObject details_Txt;
+    [SerializeField] private GameObject heartlungTxt;
+    [SerializeField] private GameObject referenceTxt;
+    [SerializeField] private BoxCollider[] boxColliders;
+
+    bool aboveTxt_OnActivated;
+    bool bottomTxt_OnActivated;
+    public int count;
+    public bool readyToTranstionSecondModel;
+
+    private Animator m_Animator;
+    Ray ray;
+    Touch touch;
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    private void Start() {
+        StartAnimation();
+        count = 0;
+    }
+
+    private void Update() {
+        RayCast_Activated();
+    }
+
+    private void StartAnimation() {
+        m_Animator = gameObject.transform.GetChild(0).GetComponent<Animator>();
+        m_Animator.SetBool("FirstRotation", true);
+    }
+
+    private Vector3 GetMousePosition() {
+        return Input.mousePosition;
+    }
+
+    public void Collider_Activated() {
+        boxColliders[0].enabled = true;
+        boxColliders[1].enabled = true;
+        details_Txt.SetActive(true);
+    }
+
+    private void RayCast_Activated() {
+
+        if (Input.GetMouseButtonDown(0)) {
+            ray = Camera.main.ScreenPointToRay(GetMousePosition());
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity)) {
+                Debug.Log("Hit: " + hit.collider.name);
+
+                Hit_Activated(hit);
+            }
+        }
+        else if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began) {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity)) {
+                    Hit_Activated(hit);
+                }
+            }
+        }
+    }
+
+    public void Hit_Activated(RaycastHit hit) {
+        if (hit.collider.tag == "Above_Text_Will Appear" && !aboveTxt_OnActivated) {
+            GetComponent<MeshRenderer>().material = cube1;
+            above_Txt.SetActive(true);
+            //bottom_Txt.SetActive(false);
+            aboveTxt_OnActivated = true;
+            count++;
+            if (count == 2) {
+                Debug.Log("count: " + count);
+                Invoke("ShaderDissolve_Activated", 3f);
+            }
+        }
+        else if (hit.collider.tag == "Bottom_Text_Will Appear" && !bottomTxt_OnActivated) {
+            GetComponent<MeshRenderer>().material = cube2;
+            //above_Txt.SetActive(false);
+            bottom_Txt.SetActive(true);
+            bottomTxt_OnActivated = true;
+            count++;
+            if (count == 2) {
+                Debug.Log("count: " + count);
+                Invoke("ShaderDissolve_Activated", 3f);
+            }
+        }
+    }
+
+    // after effect finished activate the next ui healthy lung when model changed to healthy lung...
+    // changing model another model that has a different anim.
+    // after that appears ui reference pls background.
+    public void ShaderDissolve_Activated() {
+        Debug.Log("ShaderDissolve_Activated");
+        readyToTranstionSecondModel = true;
+        
+        // show the correct model...
+        // after 3s hide it...
+
+    }
+
+    public void HideUI() {
+        above_Txt.SetActive(false);
+        bottom_Txt.SetActive(false);
+        details_Txt.SetActive(false);
+    }
+
+    public void healthyLung_Activated() {
+        // continuing the aniamtion..
+        heartlungTxt.SetActive(true);
+    }
+
+    public void healthyLung_Dectivated() {
+        // continuing the aniamtion..
+        heartlungTxt.SetActive(false);
+    }
+
+    public void ShowTheReference() {
+        referenceTxt.SetActive(true);
+    }
+}
---
+++ Assets/ShaderGraph_Dissolve/Utility/Scripts/DissolveChilds.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using static UnityEngine.Rendering.DebugUI;
+
+namespace DissolveExample
+{
+    public class DissolveChilds : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        List<Material> materials = new List<Material>();
+        public bool PingPong = false;
+        [SerializeField] SkinnedMeshRenderer broken, healthy;
+
+        public float value;
+
+        private float timer;
+
+        bool eventone;
+        bool eventtwo;
+
+        void Start()
+        {
+            //var renders = GetComponentsInChildren<Renderer>();
+            //for (int i = 0; i < renders.Length; i++)
+            //{
+                //materials.AddRange(renders[i].materials);
+            //}
+
+            
+
+            //SetValue(value);
+        }
+
+
+        public void Second_DNA_Activated() {
+            if (First_Interactable.Instance.readyToTranstionSecondModel) {
+                First_Interactable.Instance.HideUI();
+                gameObject.transform.parent.GetChild(1).gameObject.SetActive(true);
+                gameObject.SetActive(false);
+            }
+        }
+
+        public void Shader_Dissolve_Activated() {
+            Debug.Log("shader Activated 1...");
+
+            eventone = true;
+            eventtwo = false;
+            // make delay here....
+            //timer = 0;
+        }
+
+        public void Shader_Dissolve_Activated1() {
+            Debug.Log("shader Activated 2...");
+            value = 1;
+            eventone = false;
+            eventtwo = true;
+            // make delay here....
+            //timer = 0;
+        }
+
+        public void Collider_Activated() {
+            First_Interactable.Instance.Collider_Activated();
+        }
+
+        private void Reset()
+        {
+            //Start();
+            //SetValue(0);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+           
+            if (eventone) {
+
+
+                value += Time.deltaTime;//Mathf.Lerp(0, 1, Time.time * 0.100f); // Mathf.PingPong(time.time * 0.5f);
+                broken.material.SetFloat("_Dissolve", value);
+            }
+
+            if (eventtwo) {
+                value -= Time.deltaTime;
+                healthy.material.SetFloat("_Dissolve", value);
+            }
+
+        }
+
+        //IEnumerator enumerator()
+        //{
+
+        //    //float value =         while (true)
+        //    //{
+        //    //    Mathf.PingPong(value, 1f);
+        //    //    value += Time.deltaTime;
+        //    //    SetValue(value);
+        //    //    yield return new WaitForEndOfFrame();
+        //    //}
+        //}
+
+        public void SetValue(float value)
+        {
+            
+            for (int i = 0; i < materials.Count; i++) {
+                materials[i].SetFloat("_Dissolve", value);
+            }
+
+            
+        }
+
+        public void Shader_Dissolve_Dectivated() {
+            Debug.Log("shader Dectivated...");
+            eventone = true;
+        }
+
+        public void Shader_Dissolve_Dectivated1() {
+            Debug.Log("shader Dectivated...");
+            
+        }
+
+
+        public void healthyLung_Activated() {
+            // continuing the aniamtion..
+             First_Interactable.Instance.healthyLung_Activated();
+        }
+
+        public void healthyLung_Dectivated() {
+            // continuing the aniamtion..
+            First_Interactable.Instance.healthyLung_Dectivated();
+        }
+
+        public void DelayToShowTheReference() {
+            Invoke("TheReference", 3f);
+        }
+
+        private void TheReference() {
+            First_Interactable.Instance.ShowTheReference();
+        }
+    }
+}
